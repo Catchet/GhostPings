c36d755d0c8839c8e762e8979ada90007770137e
Catchet <adam.agren@gmail.com>
2025-11-03 01:28:31 +0900

    Basic S2C networking implementation

--- src/client/java/me/ghostpixels/ghostpings/GhostPingsClient.java
+++ src/client/java/me/ghostpixels/ghostpings/GhostPingsClient.java
@@ -1,10 +1,34 @@
 package me.ghostpixels.ghostpings;
 
+import me.ghostpixels.ghostpings.GhostPings.SummonLightningS2CPayload;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.util.math.BlockPos;
 
 public class GhostPingsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		// From Fabric Docs on networking
+		ClientPlayNetworking.registerGlobalReceiver(SummonLightningS2CPayload.ID, (payload, context) -> {
+			ClientWorld world = context.client().world;
+
+			if (world == null) {
+				return;
+			}
+
+			BlockPos lightningPos = payload.pos();
+			LightningEntity entity = EntityType.LIGHTNING_BOLT.create(world, SpawnReason.TRIGGERED);
+
+			if (entity != null) {
+				entity.setPosition(lightningPos.getX(), lightningPos.getY(), lightningPos.getZ());
+				world.addEntity(entity);
+			}
+		});
 	}
 }
--- src/main/java/me/ghostpixels/ghostpings/GhostPings.java
+++ src/main/java/me/ghostpixels/ghostpings/GhostPings.java
@@ -1,6 +1,18 @@
 package me.ghostpixels.ghostpings;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.block.BlockState;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +25,18 @@ public class GhostPings implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// From Fabric Docs on networking
+	public record SummonLightningS2CPayload(BlockPos pos) implements CustomPayload {
+		public static final Identifier SUMMON_LIGHTNING_PAYLOAD_ID = Identifier.of(GhostPings.MOD_ID, "summon_lightning");
+		public static final CustomPayload.Id<SummonLightningS2CPayload> ID = new CustomPayload.Id<>(SUMMON_LIGHTNING_PAYLOAD_ID);
+		public static final PacketCodec<RegistryByteBuf, SummonLightningS2CPayload> CODEC = PacketCodec.tuple(BlockPos.PACKET_CODEC, SummonLightningS2CPayload::pos, SummonLightningS2CPayload::new);
+
+		@Override
+		public Id<? extends CustomPayload> getId(){
+			return ID;
+		}
+	}
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -20,5 +44,21 @@ public class GhostPings implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		PayloadTypeRegistry.playS2C().register(SummonLightningS2CPayload.ID, SummonLightningS2CPayload.CODEC);
+
+
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+			BlockState state = world.getBlockState(pos);
+
+			if (!player.isSpectator() && player.getMainHandStack().isEmpty() && state.isToolRequired() && world instanceof ServerWorld serverWorld) {
+				player.damage(serverWorld, world.getDamageSources().generic(), 1.0F);
+				
+				SummonLightningS2CPayload payload = new SummonLightningS2CPayload(player.getBlockPos());
+				ServerPlayNetworking.send((ServerPlayerEntity) player, payload);
+			}
+
+			return ActionResult.PASS;
+		});
 	}
 }
