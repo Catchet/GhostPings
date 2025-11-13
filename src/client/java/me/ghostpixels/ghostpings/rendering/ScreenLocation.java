65f9792355f815027d411e622f0408308a8a96ec
Catchet <adam.agren@gmail.com>
2025-11-13 23:54:03 +0900

    Implemented class for working with screen coordinates

---
+++ src/client/java/me/ghostpixels/ghostpings/rendering/ScreenLocation.java
@@ -0,0 +1,31 @@
+package me.ghostpixels.ghostpings.rendering;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.util.math.Vec2f;
+
+public class ScreenLocation {
+    @Getter @Setter
+    int x;
+    @Getter @Setter
+    int y;
+    @Getter @Setter
+    int depth;
+
+    public ScreenLocation(int x, int y, int depth) {
+        this.x = x;
+        this.y = y;
+        this.depth = depth;
+    }
+
+    public boolean isInFront() {
+        return depth > 0;
+    }
+
+    public boolean isWithinBounds(Vec2f topLeft, Vec2f bottomRight) {
+        return
+            (topLeft.x <= x && x <= bottomRight.x)
+            &&
+            (topLeft.y <= y && y <= bottomRight.y);
+    }
+}
